f9f488aeb2a5ccf4508d36840feb614099a502df
Kas-Col <71794044+Kas-Col@users.noreply.github.com>
2020-11-21 15:02:54 -0700

    Finished homework assignment tasks.

---
+++ Assets/DoorMover.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class DoorMover : MonoBehaviour
+{
+    public GameObject door;
+    public GameObject player;
+
+    public AudioClip open;
+    public AudioClip close;
+
+    private Vector3 initransform;
+    private Vector3 moveTransform;
+
+    private AudioSource speaker;
+
+
+    private void Start()
+    {
+        initransform = door.transform.position;
+        moveTransform = initransform - new Vector3(0, 10, 0);
+        speaker = this.GetComponent<AudioSource>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            door.transform.position = moveTransform;
+           
+            speaker.PlayOneShot(open);
+
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            door.transform.position = initransform;
+
+            speaker.PlayOneShot(close);
+        }
+    }
+}
---
+++ Assets/SnapshotControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SnapshotControl : MonoBehaviour
+{
+
+
+    public AudioMixerSnapshot aud1;
+    public AudioMixerSnapshot aud2;
+    private bool tog;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (!tog)
+            {
+                tog = !tog;
+                aud1.TransitionTo(1);
+            }
+            else
+            {
+                tog = !tog;
+                aud2.TransitionTo(1);
+            }
+        }
+
+   
+    }
+}
