b6c4ad3e2987259b063ac48e5ce53ed65e31365c
tallbob99 <davidm1ller@shaw.ca>
2020-11-26 22:20:35 -0700

    no message

---
+++ Assets/Scripts/watersoundplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class watersoundplay : MonoBehaviour
+
+{
+    public AudioSource watersound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        watersound = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        watersound.Play();
+    }
+
+     void OnCollisionExit(Collision collision)
+    {
+        watersound.Stop();
+    }
+}
