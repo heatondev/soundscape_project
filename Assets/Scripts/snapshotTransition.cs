f97791de3f5172e849fb0370a99fbc3bcd0ef1ec
tallbob99 <davidm1ller@shaw.ca>
2020-11-27 05:15:34 -0700

    stumble sounds added. project complete

--- Assets/Scripts/snapshotTransition.cs
+++ Assets/Scripts/snapshotTransition.cs
@@ -5,13 +5,21 @@ using UnityEngine.Audio;
 
 public class snapshotTransition : MonoBehaviour
 {
+    public AudioSource stumble;
     public AudioSource AudioSource;
     public AudioMixerSnapshot fade;
     public AudioMixerSnapshot original;
     public int time;
-    
+    public bool alarmOff=false;
+  
+  
     void OnCollisionEnter(Collision collision)
     {
         fade.TransitionTo (time);
+        if (alarmOff == false)
+        { 
+            stumble.Play();
+            alarmOff = true;
+        }
     }
 }
