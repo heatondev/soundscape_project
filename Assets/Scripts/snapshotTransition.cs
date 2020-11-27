2f2edf1bb0c1727e651cb680b7a6f8582e0eeabe
tallbob99 <davidm1ller@shaw.ca>
2020-11-27 04:19:57 -0700

    alam turn off through audio mixer snapshot

---
+++ Assets/Scripts/snapshotTransition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class snapshotTransition : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public AudioMixerSnapshot fade;
+    public AudioMixerSnapshot original;
+    public int time;
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        fade.TransitionTo (time);
+    }
+}
