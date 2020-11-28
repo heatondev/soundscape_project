b9947ea4b63b769d6c9229aa0193f56764f39d6d
DESKTOP-S17ESER\seife <braden.seifert@gmail.com>
2020-11-27 19:19:34 -0800

    Hub adjustments

---
+++ Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundTrigger : MonoBehaviour
+{
+    public AudioSource LostTransmission;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LostTransmission.Play();
+        }
+    }
+}
