55cb01b9a1c57981d4117595c5de68f9e74f3c4e
DESKTOP-VTOJG77\Reeson <reesonsmith@gmail.com>
2020-11-27 21:27:37 -0700

    First Commit, made houses and added first sounds

---
+++ Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour
+{
+
+    public AudioClip triggerSound;
+    AudioSource audioSource;
+
+
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (triggerSound != null)
+        {
+            audioSource.PlayOneShot(triggerSound, 0.7f);
+
+        }
+
+    }
+
+
+}
