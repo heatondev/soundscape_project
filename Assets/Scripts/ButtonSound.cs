e66565abe6c37b40236930a8b123437df5b0f497
DESKTOP-VTOJG77\Reeson <reesonsmith@gmail.com>
2020-11-27 22:45:17 -0700

    Third Commit, Finished Project

---
+++ Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+
+    public AudioSource someSound;
+    
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (!someSound.isPlaying)
+            {
+                someSound.Play();
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.O))
+        {
+            someSound.Stop();
+        }
+    }
+
+
+
+}
