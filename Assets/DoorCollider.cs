84639985a494a0bb1e35bff1e05785a4cd107649
Savrum-zz <40185027+Savrum-zz@users.noreply.github.com>
2020-11-29 20:11:38 -0700

    Jarad Completed the project

---
+++ Assets/DoorCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCollider : MonoBehaviour
+{
+
+    public AudioSource RobotVoice;
+    public Collider DoorCol;
+
+        void OnCollisionEnter(Collision collision)
+        {
+            RobotVoice.Play();
+        }
+}
---
+++ Assets/startTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startTime : MonoBehaviour
+{
+    AudioSource myAudio;
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+        Invoke("playAudio", 15.0f);
+    }
+
+    void playAudio()
+    {
+        myAudio.Play();
+    }
+
+}
